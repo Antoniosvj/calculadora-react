364f507a13c8c4548c209d1367c2ba25ba72060e
Antoniosvj <118409406+Antoniosvj@users.noreply.github.com>
2024-09-12 23:54:00 -0300

    calculadora em react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const numeros = [7,8,9,4,5,6,1,2,3,0];
+  const operacoes = ['/', '*', '-', '+'];
+
+  const [resultado, setResultado] = useState('');
+  const [num1, setNum1] = useState(null);
+  const [operacao, setOperacao] = useState(null);
+
+  const Botao1 = ({ botaoName, onClick }) => (
+    <button className='BotaoNumero' onClick={onClick}>{botaoName}</button>
+  );
+  
+  const Botao2 = ({ botaoName, onClick }) => (
+    <button className='BotaoOperacao' onClick={onClick}>{botaoName}</button>
+  );
+
+  const numeroClicado = (numero) =>{
+    setResultado(prev => prev + numero);
+  };
+
+  const operacaoClicada = (op) =>{
+    if (resultado !== ''){
+      setNum1(parseFloat(resultado));
+      setOperacao(op);
+      setResultado('');
+    };
+  };
+
+  const igualClicado = () =>{
+    if (num1 !== null && operacao !== null && resultado !== ''){
+      const num2 = parseFloat(resultado);
+
+      const resultadoOperacao = operacao === '+' ? num1 + num2:
+                                operacao === '-' ? num1 - num2:
+                                operacao === '*' ? num1 * num2:
+                                operacao === '/' ? num1 / num2:
+                                null;
+
+      setResultado(resultadoOperacao !== null ? 
+        resultadoOperacao.toString(): 'erro');
+      setNum1(null);
+      setOperacao(null);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' className='Resultado' value={resultado} readOnly/> 
+      <div className='ContainerBotao'>
+      {numeros.map (numero =>(
+        <Botao1 
+        key={numero} 
+        botaoName={numero} 
+        onClick={() => numeroClicado(numero)}/>
+      )) }
+      <Botao2 botaoName={'AC'} onClick={() => setResultado('')}/>
+      <Botao2 botaoName={'='} onClick={() => igualClicado()}/>
+      </div>
+      <div className='CotainerBotaoOperacao'>
+        {operacoes.map (operacao =>(
+          <Botao2 
+            key={operacao} 
+            botaoName={operacao} 
+            onClick={() => operacaoClicada(operacao)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
